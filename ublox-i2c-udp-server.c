4dd32f8f52d1292fcb1b8343a53d2fd0a4647ac5
Your Name <you@example.com>
2023-03-20 14:53:32 +0000

    <gps> init使用しないので削除

--- ublox-i2c-udp-server.c
+++ ublox-i2c-udp-server.c
@@ -61,83 +61,6 @@ int readGPS(int sock,struct sockaddr_in addr)
 	return 1;
 }
 
-const unsigned char UBLOX_INIT[] = {
-	0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 0x64, 0x00, 0x01, 0x00, 0x01, 0x00, 0x7A, 0x12,				//(10Hz)
-	0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x2B, // GxGLL off
-	0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x03, 0x39, // GxGSV off
-	0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x05, 0x47, // GxVTG off
-	0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x24, // GxGGA off
-	0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x32, // GxGSA off
-};
-
-int gpsinit()
-{
-	int sendbuffer_cnt = 0;
-	char sendbuffer[512] = {0};
-
-	if (i2c_write(I2C_DEV_1, ADDR_GPS, 0xFF, UBLOX_INIT, sizeof(UBLOX_INIT)) < 0)
-	{
-		return (-1);
-	}
-	/**
-  i2c write len : 8
-	\xb5\x62\x06\x08\x00\x00\x0e\x30
-
-
-	factoryReset
-	  i2c write len : 21
-	  \xb5\x62\x06\x09\x0d\x00\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\x00\x00\xff\x17\x8a
-
-	hardReset
-	  i2c write len : 12
-	  \xb5\x62\x06\x04\x04\x00\xff\xff\x00\x00\x0c\x5d
-
-	setI2COutput(COM_TYPE_NMEA | COM_TYPE_UBX);
-	  i2c write len : 28
-	  \xb5\x62\x06\x00\x14\x00\xff\xff\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\x00\x03\x00\x00\x00\x00\x00\x1a\x2b
-
-	*/
-
-#if 0 
-  // i2cbugin
-  sendbuffer_cnt = 8;
-  memcpy(sendbuffer, "\xb5\x62\x06\x08\x00\x00\x0e\x30", sendbuffer_cnt);
-
-  if(i2c_write(I2C_DEV_1,ADDR_GPS, 0xFF, sendbuffer, sendbuffer_cnt)<0){
-		return (-1);
-	}
-
-  // factoryReset
-  sendbuffer_cnt = 21;
-  memcpy(sendbuffer, "\xb5\x62\x06\x09\x0d\x00\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\x00\x00\xff\x17\x8a", sendbuffer_cnt);
-
-  if(i2c_write(I2C_DEV_1,ADDR_GPS, 0xFF, sendbuffer, sendbuffer_cnt)<0){
-		return (-1);
-	}
-
-  // hardReset
-  sendbuffer_cnt = 12;
-  memcpy(sendbuffer, "\xb5\x62\x06\x04\x04\x00\xff\xff\x00\x00\x0c\x5d", sendbuffer_cnt);
-
-  if(i2c_write(I2C_DEV_1,ADDR_GPS, 0xFF, sendbuffer, sendbuffer_cnt)<0){
-		return (-1);
-	}
-
-  sleep(5);
-
-  // setI2COutput(COM_TYPE_NMEA | COM_TYPE_UBX);
-  sendbuffer_cnt = 28;
-  memcpy(sendbuffer, "\xb5\x62\x06\x00\x14\x00\xff\xff\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\x00\x03\x00\x00\x00\x00\x00\x1a\x2b", sendbuffer_cnt);
-
-  if(i2c_write(I2C_DEV_1,ADDR_GPS, 0xFF, sendbuffer, sendbuffer_cnt)<0){
-		return (-1);
-	}
-#endif
-
-	//  sendbuffer_cnt = 28;
-	// memcpy(sendbuffer, "\xb5\x62\x06\x00\x14\x00\xff\xff\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\x00\x03\x00\x00\x00\x00\x00\x1a\x2b", sendbuffer_cnt);
-}
-
 int endflag = 0;
 void SignalHandler(int signo)
 {
@@ -148,7 +71,6 @@ int main(int argc, char *argv[])
 	signal(SIGUSR1, SignalHandler);
 	signal(SIGINT, SignalHandler);
 	signal(SIGTERM, SignalHandler);
-	// gpsinit();
 
 	int sock;
 	struct sockaddr_in addr;
